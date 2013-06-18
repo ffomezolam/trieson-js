700e7068db015d2de0567972d9bce0775b46e76b
ffomezolam <ffomezolam@gmail.com>
2013-06-17 20:09:02 -0400

    not using requireJS format any more

--- trieson.js
+++ trieson.js
@@ -3,75 +3,71 @@
  *
  * @module trieson
  */
-(function(name, context, definition) {
-    if(typeof module !== 'undefined' && module.exports) module.exports = definition(/*require(deps)*/);
-    else if(typeof define === 'function' && define.amd) define(/*[deps], */definition);
-    else context[name] = definition();
-})('Trieson', this, function(/*deps*/) {
+
+var triesonNode = require('./triesonNode');
+
+/**
+ * Trie class
+ * TODO: set up initialization method
+ *
+ * @class Trieson
+ * @constructor
+ */
+function Trieson() {
+    this._root = new triesonNode();
+}
+
+Trieson.prototype = {
     /**
-     * Trie class
-     * TODO: set up initialization method
+     * Test whether a string is in the trie
      *
-     * @class Trieson
-     * @constructor
+     * @method has
+     * @param {String} s String to test
+     * @return {Boolean} Whether string is in trie
      */
-    function Trieson() {
-        this._root = null;
-        this._nodeType = null;
-    }
+    has: function(s) {
+        return !!this.get(s);
+    },
 
-    Trieson.prototype = {
-        /**
-         * Test whether a string is in the trie
-         *
-         * @method has
-         * @param {String} s String to test
-         * @return {Boolean} Whether string is in trie
-         */
-        has: function(s) {
-            return !!this.get(s);
-        },
+    /**
+     * Add a string to the trie
+     *
+     * @method add
+     * @chainable
+     * @param {String} s String to add
+     * @param {any} [d] Data to associate with string
+     */
+    add: function(s, d) {
+        var i,
+            l = s.length,
+            n = this._root;
 
-        /**
-         * Add a string to the trie
-         *
-         * @method add
-         * @chainable
-         * @param {String} s String to add
-         * @param {any} [d] Data to associate with string
-         */
-        add: function(s, d) {
-            var i,
-                l = s.length,
-                n = this._root;
-
-            for (i = 0; i < l; i++) {
-                var c = s.charAt(i);
-                n = n._addChild(c)._getChild(c);
-            }
-
-            n.value = d || true;
-
-            return this;
-        },
-
-        /**
-         * Get data associated with string
-         *
-         * @method get
-         * @param {String} s String to query
-         * @return {any} Data associated with string
-         */
-        get: function(s) {
-            var n = this._root;
-            for (var i = 0, l = s.length; i < l; i ++) {
-                var c = s.charAt(i);
-                if(!n._hasChild(c)) return null;
-                n = n._getChild(c);
-            }
-            return n.value;
+        for (i = 0; i < l; i++) {
+            var c = s.charAt(i);
+            n = n.add(c).get(c);
         }
-    };
 
-    return Trieson;
-});
+        n.value = d || true;
+
+        return this;
+    },
+
+    /**
+     * Get data associated with string
+     *
+     * @method get
+     * @param {String} s String to query
+     * @return {any} Data associated with string
+     */
+    get: function(s) {
+        var n = this._root;
+        for (var i = 0, l = s.length; i < l; i ++) {
+            var c = s.charAt(i);
+            if(!n.has(c)) return null;
+            n = n.get(c);
+        }
+        return n.value;
+    }
+};
+
+module.exports = Trieson;
--- triesonNode.js
+++ triesonNode.js
@@ -1,89 +1,84 @@
 /**
  * Basic trie implementation
  *
- * @module triesonNode
+ * @module trieson
  */
-(function(name, context, definition) {
-    if(typeof module !== 'undefined' && module.exports) module.exports = definition(/*require(deps)*/);
-    else if(typeof define === 'function' && define.amd) define(/*[deps], */definition);
-    else context[name] = definition();
-})('TriesonNode', this, function(/*deps*/) {
+
+/**
+ * Trie node class
+ *
+ * @class TriesonNode
+ * @constructor
+ */
+function TriesonNode() {
+    this.value = null;
+    this.children = {};
+}
+
+TriesonNode.prototype = {
     /**
-     * Trie node class
+     * Add a character to the trie
      *
-     * @class TriesonNode
-     * @constructor
+     * @method add
+     * @private
+     * @chainable
+     * @param {String} c Character to add
      */
-    function TriesonNode() {
-        this.value = null;
-        this.children = {};
+    add: function(c) {
+        if(c == null) return this;
+        if(!this.children[c]) this.children[c] = new TriesonNode();
+        return this;
+    },
+
+    /**
+     * Get a node from the trie
+     *
+     * @method get
+     * @private
+     * @param {String} c Character to get
+     * @return {Trieson} Node associated with character
+     */
+    get: function(c) {
+        if(this.children[c] == null) return null;
+        return this.children[c];
+    },
+
+    /**
+     * Remove a character from the trie
+     *
+     * @method remove
+     * @private
+     * @chainable
+     * @param {String} c Character to remove
+     */
+    remove: function(c) {
+        if(this.children[c] == null) return this;
+        delete this.children[c];
+        return this;
+    },
+
+    /**
+     * Test if child node exists
+     *
+     * @method has
+     * @private
+     * @param {String} c Character to test
+     * @return {Boolean} Whether child exists
+     */
+    has: function(c) {
+        return this.children[c] != null;
+    },
+
+    /**
+     * Test whether a string is in the trie
+     *
+     * @method has
+     * @param {String} s String to test
+     * @return {Boolean} Whether string is in trie
+     */
+    has: function(s) {
+        return !!this.get(s);
     }
+};
 
-    TriesonNode.prototype = {
-        /**
-         * Add a character to the trie
-         *
-         * @method add
-         * @private
-         * @chainable
-         * @param {String} c Character to add
-         */
-        add: function(c) {
-            if(c == null) return this;
-            if(!this.children[c]) this.children[c] = new TriesonNode();
-            return this;
-        },
-
-        /**
-         * Get a node from the trie
-         *
-         * @method get
-         * @private
-         * @param {String} c Character to get
-         * @return {Trieson} Node associated with character
-         */
-        get: function(c) {
-            if(this.children[c] == null) return null;
-            return this.children[c];
-        },
-
-        /**
-         * Remove a character from the trie
-         *
-         * @method remove
-         * @private
-         * @chainable
-         * @param {String} c Character to remove
-         */
-        remove: function(c) {
-            if(this.children[c] == null) return this;
-            delete this.children[c];
-            return this;
-        },
-
-        /**
-         * Test if child node exists
-         *
-         * @method has
-         * @private
-         * @param {String} c Character to test
-         * @return {Boolean} Whether child exists
-         */
-        has: function(c) {
-            return this.children[c] != null;
-        },
-
-        /**
-         * Test whether a string is in the trie
-         *
-         * @method has
-         * @param {String} s String to test
-         * @return {Boolean} Whether string is in trie
-         */
-        has: function(s) {
-            return !!this.get(s);
-        }
-    };
-
-    return TriesonNode;
-});
+module.exports = TriesonNode;
