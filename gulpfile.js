f89ad344c73c46c894e8a9e426f335873f182558
Andrew Malozemoff <ffomezolam@gmail.com>
2014-09-21 17:03:08 -0400

    updated gulpfile for unit testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename')
+    rename = require('gulp-rename'),
+    mocha = require('gulp-mocha');
 
-gulp.task('minify', function() {
+gulp.task('default', function() {
+    gulp.src('test/*.js', { read: false })
+        .pipe(mocha({ reporter: 'spec' }))
+
     gulp.src('trieson.js')
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
