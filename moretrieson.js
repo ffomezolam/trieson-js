d24077d9a2e30333252332f4e69eba7fbc226e55
ffomezolam <ffomezolam@gmail.com>
2014-09-28 20:23:04 -0400

    Lots of edits

--- moretrieson.js
+++ moretrieson.js
@@ -4,10 +4,10 @@
  * @module trieson
  */
 (function(name, context, definition) {
-    if(typeof module !== 'undefined' && module.exports) module.exports = definition(require('trieson'));
-    else if(typeof define === 'function' && define.amd) define(['trieson'], definition);
-    else context[name] = definition(Trieson);
-})("MoreTrieson", this, function(Trieson) {
+    if(typeof module !== 'undefined' && module.exports) module.exports = definition();
+    else if(typeof define === 'function' && define.amd) define(definition);
+    else context[name] = definition();
+})("MoreTrieson", this, function() {
     // TODO: There's an error in this because same character gets entered
     // multiple times. Fix it.
     function RandomContainer() {
@@ -27,34 +27,38 @@
         return this._data[rn];
     };
 
-    function TriesonNode() {
+    function TriesonNode(c) {
+        this._container = c;
         this._data = null;
     }
 
     TriesonNode.prototype.add = function(c) {
         if(!(c in this)) {
-            this[c] = new TriesonNode();
-            if(!this._data) this._data = new RandomContainer();
+            this[c] = new TriesonNode(this._container);
+            if(!this._data) this._data = new this._container();
             this._data.add(c);
-            console.log('added', c);
         }
+
         return this;
     };
 
     TriesonNode.prototype.get = function(c) {
+        if(typeof c == 'undefined') return this[this._data.get()];
         return this[c];
     };
 
-    TriesonNode.prototype.map = function(fn) {
+    TriesonNode.prototype.each = function(fn) {
         if(!this._data || !this._data.length) return;
-        console.log('mapping', this);
         for(var i = 0, l = this._data.length; i < l; i++) {
-            var c = this._data[i],
+            var c = this._data.get(i),
                 n = this.get(c);
-            console.log(c);
             fn(c);
             n.map(fn);
         }
+    };
+
+    TriesonNode.prototype.isTerminal = function() {
+        return !this._data;
     };
 
     /**
@@ -65,8 +69,19 @@
      * @param {Object} [opts] Options
      */
     function MoreTrieson(opts) {
-        this._root = new TriesonNode();
-        this._depth = 3;
+        this._container = (opts && 'container' in opts) ? opts.container : RandomContainer;
+        this._root = new TriesonNode(this._container);
+        this._depth = (opts && 'depth' in opts) ? opts.depth : 3;
+        this._chars = {
+            start: '^',
+            end: '$'
+        };
+        this._opts = {
+            searchDepth: (opts && 'searchDepth' in opts) ? opts.searchDepth : this._depth,
+            maxLength: (opts && 'maxLength' in opts) ? opts.maxLength : 10,
+            minLength: (opts && 'minLength' in opts) ? opts.minLength : 3,
+
+        };
     }
 
     /**
@@ -80,22 +95,37 @@
         var a = s.split(''),
             n = this._root;
 
-        n.add('^');
-        n = n.get('^');
+        n.add(this._chars.start);
+        n = n.get(this._chars.start);
 
         for(var i = 0, j = 0, l = a.length; i < l; i++, j = i % this._depth) {
             n.add(a[i]);
             n = n.get(a[i]);
         }
 
-        n.add('$');
+        n.add(this._chars.end);
 
         return this;
     };
 
-    MoreTrieson.prototype.all = function() {
+    /**
+     * Get a string from the trie
+     *
+     * @method get
+     * @return {String} String or null if doesn't exist
+     */
+    MoreTrieson.prototype.get = function(s) {
         var n = this._root;
-        n.map(console.log);
+
+        for(var i = 0, l = s.length; i < l; i++) {
+        }
+    };
+
+    MoreTrieson.prototype.all = function() {
+        var n = this._root,
+            words = [];
+        n.map(function(c) {
+        });
     };
 
     return MoreTrieson;
